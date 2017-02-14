4ec0be2089499b8fe628914179d9a0495e22068a
benji <benji.b.ramirez@gmail.com>
2017-02-13 22:04:46 -0800

    modified dan  shiffman's code to make wavy flow field

--- static/js/noise/perlin_branches.js
+++ static/js/noise/perlin_branches.js
@@ -1,6 +1,6 @@
 /**
  * Created by benji on 2/12/17.
- * modified version of Daniel Shiffman's video on perlin noise with P5 JS
+ * modified version of Daniel Shiffman's video on perlin noise with P5 JS,
  *  find him here:
  *   http://patreon.com/codingtrain
  *  https://youtu.be/BjoM9oKOAKY
@@ -9,68 +9,76 @@
 
 
 function Particle() {
-  this.pos = createVector(random(width), random(height));
-  this.vel = createVector(0, 0);
-  this.acc = createVector(0, 0);
-  this.maxspeed = 4;
-  this.h = 0;
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxspeed = 4;
+    this.h = 100;
+    this.h = true;
 
-  this.prevPos = this.pos.copy();
+    this.prevPos = this.pos.copy();
 
-  this.update = function() {
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxspeed);
-    this.pos.add(this.vel);
-    this.acc.mult(1); //retain some of the previous acceleration
-  }
-
-  this.follow = function(vectors) {
-    var x = floor(this.pos.x / scl);
-    var y = floor(this.pos.y / scl);
-    var index = x + y * cols;
-    var force = vectors[index];
-    this.applyForce(force);
-  }
-
-  this.applyForce = function(force) {
-    this.acc.add(force);
-  }
-
-  this.show = function() {
-    stroke(this.h, 255, 255, 25);
-    this.h = this.h + 1;
-    if (this.h > 255) {
-      this.h = 0;
-    }
-    strokeWeight(1);
-    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-    this.updatePrev();
-  }
-
-  this.updatePrev = function() {
-    this.prevPos.x = this.pos.x;
-    this.prevPos.y = this.pos.y;
-  }
-
-  this.edges = function() {
-    if (this.pos.x > width) {
-      this.pos.x = 0;
-      this.updatePrev();
-    }
-    if (this.pos.x < 0) {
-      this.pos.x = width;
-      this.updatePrev();
-    }
-    if (this.pos.y > height) {
-      this.pos.y = 0;
-      this.updatePrev();
-    }
-    if (this.pos.y < 0) {
-      this.pos.y = height;
-      this.updatePrev();
+    this.update = function() {
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxspeed);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
     }
 
-  }
+    this.follow = function(vectors) {
+        var x = floor(this.pos.x / scl);
+        var y = floor(this.pos.y / scl);
+        var index = x + y * cols;
+        var force = vectors[index];
+        this.applyForce(force);
+    }
+
+    this.applyForce = function(force) {
+        this.acc.add(force);
+    }
+
+    this.show = function() {
+        stroke(this.h, 255, 255, 25);
+        if (this.dir == true){
+            this.h = this.h +1;
+        }else{
+            this.h = this.h - 1;
+        }
+        if (this.h >= 200){
+            this.dir = false;
+        }
+        if(this.h <= 100){
+            this.dir = true
+        }
+        strokeWeight(4);
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+        this.updatePrev();
+    }
+
+    this.updatePrev = function() {
+        this.prevPos.x = this.pos.x;
+        this.prevPos.y = this.pos.y;
+    }
+
+    this.edges = function() {
+        if (this.pos.x > width) {
+            this.pos.x = 0;
+            this.updatePrev();
+        }
+        if (this.pos.x < 0) {
+            this.pos.x = width;
+            this.updatePrev();
+        }
+        if (this.pos.y > height) {
+            this.pos.y = 0;
+            this.updatePrev();
+        }
+        if (this.pos.y < 0) {
+            this.pos.y = height;
+            this.updatePrev();
+        }
+
+    }
 
 }
 
@@ -79,63 +87,88 @@ function Particle() {
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/BjoM9oKOAKY
 
-var inc = 0.1;
-var scl = 10;
 var cols, rows;
 
 var zoff = 0;
 
 var fr;
-
+var incr = 0.01;
 var particles = [];
+var scl = 10;
 
-var flowfield;
+
+
+var flowfield = [];
+
+
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  colorMode(HSB, 255);
-  cols = floor(width / scl);
-  rows = floor(height / scl);
-  fr = createP('');
+    background(4);
+    createCanvas(800, 800);
+    colorMode(HSB, 255);
 
-  flowfield = new Array(cols * rows);
 
-  for (var i = 0; i < 300; i++) {
-    particles[i] = new Particle();
-  }
-  background(51);
+    cols = floor(width / scl);
+    rows = floor(height / scl);
+    fr = createP('');
+
+
+    flowfield = new Array(cols * rows);
+    //
+    for (var i = 0; i < 1000; i++) {
+        particles[i] = new Particle();
+    }
+    fr = createP('');
+
+
+
+
+    // particles[0] = new Particle();
 }
 
 function draw() {
-  var yoff = 0;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      var index = x + y * cols;
-      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
-      var v = p5.Vector.fromAngle(angle);
-      v.setMag(10000);
-      flowfield[index] = v;
-      xoff += inc;
-      stroke(0, 50);
-      // push();
-      // translate(x * scl, y * scl);
-      // rotate(v.heading());
-      // strokeWeight(1);
-      // line(0, 0, scl, 0);
-      // pop();
+
+    randomSeed(10);
+
+    var yoff = 0;
+
+    console.log(noise(1));
+
+
+    for (var y = 0; y < rows; y++){
+
+        var xoff = 0;
+
+        for (var x = 0; x < cols; x++){
+            var index = (x + y * cols);
+            var angle = noise(xoff, yoff, zoff) * TWO_PI;
+
+            var v = p5.Vector.fromAngle(angle);
+            flowfield[index] = v;
+
+            xoff += incr;
+
+            // stroke(0,50);
+            // push();
+            // translate(x*scl, y * scl);
+            // rotate(v.heading());
+            // line(0, 0, scl, 0);
+            //
+            // pop();
+
+        }
+        yoff += incr;
+
+        zoff += 0.0001;
     }
-    yoff += inc;
 
-    zoff += 0.0003;
-  }
+    for (var i = 0; i < particles.length; i ++){
+        particles[i].follow(flowfield);
+        particles[i].update();
+        particles[i].show();
+        particles[i].edges();
+    }
+    fr.html(floor(frameRate()));
 
-  for (var i = 0; i < particles.length; i++) {
-    particles[i].follow(flowfield);
-    particles[i].update();
-    particles[i].edges();
-    particles[i].show();
-  }
 
-  fr.html(floor(frameRate()));
 }
