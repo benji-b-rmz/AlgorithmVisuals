a952640c2c1daac69f04f9ea97617c2c9003d2fc
benji <benji.b.ramirez@gmail.com>
2017-02-23 21:20:24 -0800

    added update function for three js attractor, added attractor at origin

--- static/js/physics/3JSattractor.js
+++ static/js/physics/3JSattractor.js
@@ -10,6 +10,8 @@ var particles = [];
 var numParticles = 100;
 var shapeSize = 3;
 
+var attractor;
+
 
 init();
 animate();
@@ -21,18 +23,53 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function addSpotLight(x, y, z, color) {
+
+    var spotLight = new THREE.SpotLight(color);
+    spotLight.position.set(x, y, z);
+    spotLight.castShadow = false;
+    scene.add(spotLight);
+
+}
+
+function createBackground(x, y, z, color){
+    var geom = new THREE.BoxGeometry(x, y, z);
+    var material = new THREE.MeshLambertMaterial({
+        color: color,
+        wireframe: false
+    });
+    var background = new Particle(geom, material, 0, 0, 0); // at origin
+    background.addToScene();
+}
 
 function Particle(shape, material, x, y, z){
 
     this.mesh = new THREE.Mesh(shape, material);
     this.mesh.position.set(x, y, z);
 
+    //set physics properties velocity, acceleration,
+    this.vel = new THREE.Vector3(0, 0, 0);
+    this.acc = new THREE.Vector3(0, 0, 0);
+
+    this.update = function () {
+
+        this.vel.add(this.acc);
+        
+        this.mesh.position.add(this.vel); //update the position
+
+        this.acc.multiplyScalar(0);
+    };
+
+    this.applyForce = function(forceVector){
+        this.acc.add(forceVector);
+    };
+
     this.addToScene = function () {
 
         scene.add(this.mesh);
         console.log(this.mesh.position);
 
-    }
+    };
 
 }
 
@@ -41,14 +78,12 @@ function createParticles(numP){
     for ( var i = 0; i < numP; i ++){
 
         var shape = new THREE.BoxGeometry(shapeSize, shapeSize, shapeSize);
-        var material = new THREE.MeshBasicMaterial({
+        var material = new THREE.MeshLambertMaterial({
             color: 0xff00ff, wireframe:false
         });
         var x = getRandomInt(-50, 50),
             y = getRandomInt(-50, 50),
             z = getRandomInt(-50, 50);
-
-
 
         var particle = new Particle(shape, material, x, y, z );
         particle.addToScene();
@@ -63,16 +98,15 @@ function init(){
     //set up the scene, camera, lights, controls, and renderer
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x111111 );
 
     //the camera obj parameters = (FoV, aspect ratio, znear, zfar)
     camera = new THREE.PerspectiveCamera(90, (window.innerWidth/window.innerHeight), 0.1, 10000);
     camera.position.z = 100;
 
-    //lights, positioned above initially
-    //var spotLight = new THREE.SpotLight(0xffffff);
-    // spotLight.position.set(0,50,0);
-    // spotLight.castShadow = false;
-    // scene.add(spotLight);
+    // lights, positioned above initially
+    addSpotLight(0, 200, 0, 0xffffff);
+    addSpotLight(0, 0, 200, 0xffffff);
 
 
     renderer = new THREE.WebGLRenderer();
@@ -88,8 +122,18 @@ function init(){
 
 
     // now create and add the objects to the scene
-
     createParticles(numParticles);
+
+    attractor = new THREE.Vector3(0,0,0);
+    for (var i = 0; i < particles.length; i ++){
+
+        var forceVec = new THREE.Vector3( attractor.x - particles[i].mesh.position.x,
+                                        attractor.y - particles[i].mesh.position.y,
+                                        attractor.z - particles[i].mesh.position.z);
+        particles[i].applyForce(forceVec);
+
+    }
+
 
 
     // add the canvas to the webpage:
@@ -102,6 +146,12 @@ function animate() {
     requestAnimationFrame( animate ); //call this function up to 60 times per second
 
     controls.update(); //recalc camera based on control input
+
+    for ( var i = 0; i < particles.length; i ++){
+
+        particles[i].update();
+
+    }
 
     renderer.render( scene, camera );
 
