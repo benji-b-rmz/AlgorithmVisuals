24e41691e92ebaed8dbe8082c42815de59dd37f5
benji <benji.b.ramirez@gmail.com>
2017-03-31 20:43:13 -0700

    started reaction time page

---
+++ static/js/interactive/reactionTime.js
@@ -0,0 +1,50 @@
+/**
+ * Created by benji on 3/31/17.
+ */
+
+var NUM_PARTICLES = 200;
+var PARTICLES = [];
+var F_G; // simulated force of gravity -y force
+var START_TIME; //holds the amount of time before the start of the game(random)
+
+
+function reactionCanvas(){
+
+    this.ready = false; //initially false, used to detect early clicks
+    this.color = 0x00ffff; //Initially purple,
+    this.text = "Wait for it...";
+
+
+}
+
+function centerCanvas() {
+
+    var x = (windowWidth - width)/2;
+    var y = (windowHeight - height)/2;
+    cnv.position(x, y);
+
+}
+
+function windowResized(){
+    setup();
+    centerCanvas();
+}
+
+function setup() {
+
+    background(0);
+    F_G = createVector(0, 9.8);
+    cnv = createCanvas(window.innerWidth, window.innerHeight);
+    centerCanvas();
+
+    //initialize the random start time
+    START_TIME = Math.floor((Math.random() * 5000) + 1000); //canvas will turn green within
+
+}
+
+function draw() {
+
+
+
+
+}
